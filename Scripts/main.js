b1cd22ee3bd93142d09c16b876af985e46ff85ff
alexsameri <samerialexx@gmail.com>
2023-04-12 18:21:05 +0300

    javascript

---
+++ Scripts/main.js
@@ -0,0 +1,3 @@
+//The heading text changed to Hello world! using JavaScript. You did this by using a function called querySelector()
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
